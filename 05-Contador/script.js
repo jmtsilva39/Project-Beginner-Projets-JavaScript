f01bdb5a3508467347a2c10cc7d93814327fb763
Jonez de Mello Toffani da Silva <jmts39@hotmail.com>
2023-10-27 16:53:25 -0300

    contador
    finalizado

---
+++ 05-Contador/script.js
@@ -0,0 +1,15 @@
+var count = 0;
+
+
+function somar(number){
+    count += number;
+    document.getElementById("count").innerHTML = count;
+}
+
+function subtrair(number){
+    count += number;
+    document.getElementById("count").innerHTML = count;
+}
+
+
+
